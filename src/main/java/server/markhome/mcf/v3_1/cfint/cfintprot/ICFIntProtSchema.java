c106e69d8960f5937dd29fd90fcaa9acc481be29
Mark Sobkow <mark.sobkow@gmail.com>
2026-06-30 08:12:21 -0600

    2026-06-30 01:25 CST Started in on the Prot[ected] layer...

---
+++ src/main/java/server/markhome/mcf/v3_1/cfint/cfintprot/ICFIntProtSchema.java
@@ -0,0 +1,551 @@
+// Description: Java 25 public interface for a CFInt schema.
+
+/*
+ *	server.markhome.mcf.CFInt
+ *
+ *	Copyright (c) 2016-2026 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal 3.1 CFInt - Internet Essentials
+ *	
+ *	This file is part of Mark's Code Fractal CFInt.
+ *	
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *	
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *	
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ *	
+ */
+
+package server.markhome.mcf.v3_1.cfint.cfintprot;
+
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.math.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.*;
+import java.util.*;
+import server.markhome.mcf.v3_1.cflib.*;
+import server.markhome.mcf.v3_1.cflib.dbutil.*;
+import server.markhome.mcf.v3_1.cflib.xml.CFLibXmlUtil;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.text.StringEscapeUtils;
+import java.util.concurrent.atomic.*;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import server.markhome.mcf.v3_1.cfsec.cfsecprot.*;
+
+import server.markhome.mcf.v3_1.cfsec.cfsecpub.*;
+import server.markhome.mcf.v3_1.cfint.cfintpub.*;
+import server.markhome.mcf.v3_1.cfsec.cfsecprot.*;$importJavaProtPackageSchemaNameBuffDotFactoryService$
+//import server.markhome.mcf.v3_1.cfint.cfintprot.buff.CFIntProtBuffSchema;
+//import server.markhome.mcf.v3_1.cfint.cfintprot.buff.CFIntProtBuffFactoryService;
+
+public interface ICFIntProtSchema
+extends ICFSecProtSchema
+{
+
+	public static final String SCHEMA_NAME = "CFInt";
+	public static final String DBSCHEMA_NAME = "CFInt31";
+	static final AtomicReference<ApplicationContext> arApplicationContext = new AtomicReference<>(null);
+	public static final CFSecPubTableData TABLE_DATA[] = {new CFSecPubTableData("CFInt", "License", null, false, false, "Tenant", "Public"),
+		new CFSecPubTableData("CFInt", "MajorVersion", null, true, false, "Tenant", "Public"),
+		new CFSecPubTableData("CFInt", "MimeType", null, true, false, "System", "Public"),
+		new CFSecPubTableData("CFInt", "MinorVersion", null, true, false, "Tenant", "Public"),
+		new CFSecPubTableData("CFInt", "SubProject", null, true, false, "Tenant", "Public"),
+		new CFSecPubTableData("CFInt", "Tld", null, true, false, "Tenant", "Public"),
+		new CFSecPubTableData("CFInt", "TopDomain", null, true, false, "Tenant", "Public"),
+		new CFSecPubTableData("CFInt", "TopProject", null, true, false, "Tenant", "Public"),
+		new CFSecPubTableData("CFInt", "URLProtocol", null, true, false, "System", "Public")};
+	public static final AtomicReference<CFSecPubTableData[]> consolidatedProtTableData = new AtomicReference<>(null);
+	public static final CFSecPubRoleInfo ROLE_INFO[] = {};
+	public static final AtomicReference<CFSecPubRoleInfo[]> consolidatedProtRoleInfo = new AtomicReference<>(null);
+	public static CFSecPubTableData[] getProtTableData() {
+		return TABLE_DATA;
+	}
+
+	public static CFSecPubTableData[] getConsolidatedProtTableData() {
+		if (consolidatedProtTableData.get() == null) {
+			ArrayList<CFSecPubTableData> lst = new ArrayList<>();
+			for( CFSecPubTableData data: ICFSecProtSchema.getTableData()) {
+				lst.add(data);
+			}
+			for( CFSecPubTableData data: TABLE_DATA) {
+				lst.add(data);
+			}
+			CFSecPubTableData arr[] = new CFSecPubTableData[lst.size()];
+			int idx = 0;
+			for(CFSecPubTableData data: lst) {
+				arr[idx++] = data;
+			}
+			consolidatedProtTableData.compareAndSet(null, arr);
+		}
+		return(consolidatedProtTableData.get());
+	}
+
+	public static CFSecPubRoleInfo[] getProtRoleInfo() {
+		return ROLE_INFO;
+	}
+
+	public static CFSecPubRoleInfo[] getConsolidatedProtRoleInfo() {
+		if (consolidatedProtRoleInfo.get() == null) {
+			ArrayList<CFSecPubRoleInfo> lst = new ArrayList<>();
+			for( CFSecPubRoleInfo info: ICFSecProtSchema.getRoleInfo()) {
+				lst.add(info);
+			}
+			for( CFSecPubRoleInfo info: ROLE_INFO) {
+				lst.add(info);
+			}
+			// Dependency order is the natural order of role info comparison
+			lst.sort(new CFSecPubRoleInfoDependencyComparator());
+			CFSecPubRoleInfo arr[] = new CFSecPubRoleInfo[lst.size()];
+			int idx = 0;
+			for(CFSecPubRoleInfo info: lst) {
+				arr[idx++] = info;
+			}
+			consolidatedProtRoleInfo.compareAndSet(null, arr);
+		}
+		return(consolidatedProtRoleInfo.get());
+	}
+
+	public default void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+		arApplicationContext.compareAndSet(arApplicationContext.get(), applicationContext);
+	}
+
+	public static ApplicationContext getApplicationContext() {
+		return( arApplicationContext.get() );
+	}
+
+		final static ArrayList<ICFSecProtSchema.ClassMapEntry> entries = new ArrayList<>();
+		final static HashMap<Integer,ICFSecProtSchema.ClassMapEntry> mapBackingClassCodeToEntry = new HashMap<>();
+		final static HashMap<Integer,ICFSecProtSchema.ClassMapEntry> mapRuntimeClassCodeToEntry = new HashMap<>();
+		final static AtomicReference<ICFIntProtSchema> backingCFIntProtSchema = new AtomicReference<>();
+		public static ICFIntProtSchema getBackingCFIntProtSchema() {
+			return( ICFIntProtSchema.backingCFIntProtSchema.get() );
+		}
+		
+		public static void setBackingCFIntProtSchema(ICFIntProtSchema backingSchema) {
+			ICFIntProtSchema.backingCFIntProtSchema.set(backingSchema);
+		}
+		
+		public ICFIntProtSchema getCFIntProtSchema();
+		public void setCFIntProtSchema(ICFIntProtSchema schema);
+		
+		public static int doInitClassMapEntries(int value) {
+			value = ICFSecProtSchema.doInitClassMapEntries(value);
+			if (ICFIntProtSchema.entries.isEmpty()) {
+				ICFSecProtSchema.ClassMapEntry entry;
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "License", ICFIntProtLicense.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "MajorVersion", ICFIntProtMajorVersion.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "MimeType", ICFIntProtMimeType.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "MinorVersion", ICFIntProtMinorVersion.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "SubProject", ICFIntProtSubProject.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "Tld", ICFIntProtTld.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "TopDomain", ICFIntProtTopDomain.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "TopProject", ICFIntProtTopProject.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				entry = new ICFSecProtSchema.ClassMapEntry(ICFIntProtSchema.SCHEMA_NAME, "URLProtocol", ICFIntProtURLProtocol.CLASS_CODE);
+				ICFIntProtSchema.entries.add(entry);
+				for( ICFSecProtSchema.ClassMapEntry cur: ICFIntProtSchema.entries) {
+					cur.setRuntimeClassCode(value++);
+				}
+				ICFIntProtSchema.mapBackingClassCodeToEntry.clear();
+				ICFIntProtSchema.mapRuntimeClassCodeToEntry.clear();
+				for( ICFSecProtSchema.ClassMapEntry cur: ICFIntProtSchema.entries) {
+					ICFIntProtSchema.mapBackingClassCodeToEntry.put(cur.getBackingClassCode(), cur);
+					ICFIntProtSchema.mapRuntimeClassCodeToEntry.put(cur.getRuntimeClassCode(), cur);
+				}
+			}
+			return(value);
+		}
+		
+		public static ICFSecProtSchema.ClassMapEntry getClassMapByBackingClassCode(int code) {
+			ICFSecProtSchema.ClassMapEntry entry;
+			entry = ICFSecProtSchema.mapBackingClassCodeToEntry.get(code);
+			if (entry != null) {
+				return( entry );
+			}
+			entry = ICFIntProtSchema.mapBackingClassCodeToEntry.get(code);
+			if (entry != null) {
+				return( entry );
+			}
+			return( null );
+		}
+		
+		public static ICFSecProtSchema.ClassMapEntry getClassMapByRuntimeClassCode(int code) {
+			ICFSecProtSchema.ClassMapEntry entry;
+			entry = ICFSecProtSchema.mapRuntimeClassCodeToEntry.get(code);
+			if (entry != null) {
+				return( entry );
+			}
+			entry = ICFIntProtSchema.mapRuntimeClassCodeToEntry.get(code);
+			if (entry != null) {
+				return( entry );
+			}
+			return( null );
+		}
+		
+		public int initClassMapEntries(int value);
+		public void wireRecConstructors();
+		public void wireTableTableInstances();
+
+	/**
+	 *	Allocate a new schema instance.
+	 *
+	 *	@return	A new ICFSecProtSchema instance.
+	 */
+	public ICFSecProtSchema newSchema();
+
+	/**
+	 *	Get the next ISOCcyIdGen identifier.
+	 *
+	 *	@return	The next ISOCcyIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public short nextISOCcyIdGen();
+
+	/**
+	 *	Get the next ISOCtryIdGen identifier.
+	 *
+	 *	@return	The next ISOCtryIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public short nextISOCtryIdGen();
+
+	/**
+	 *	Get the next ISOLangIdGen identifier.
+	 *
+	 *	@return	The next ISOLangIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public short nextISOLangIdGen();
+
+	/**
+	 *	Get the next ISOTZoneIdGen identifier.
+	 *
+	 *	@return	The next ISOTZoneIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public short nextISOTZoneIdGen();
+
+	/**
+	 *	Get the next TableInfoIdGen identifier.
+	 *
+	 *	@return	The next TableInfoIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public int nextTableInfoIdGen();
+
+	/**
+	 *	Get the next MimeTypeIdGen identifier.
+	 *
+	 *	@return	The next MimeTypeIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public int nextMimeTypeIdGen();
+
+	/**
+	 *	Get the next URLProtocolIdGen identifier.
+	 *
+	 *	@return	The next URLProtocolIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public int nextURLProtocolIdGen();
+
+	/**
+	 *	Get the next ClusterIdGen identifier.
+	 *
+	 *	@return	The next ClusterIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextClusterIdGen();
+
+	/**
+	 *	Get the next SecSessionIdGen identifier.
+	 *
+	 *	@return	The next SecSessionIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextSecSessionIdGen();
+
+	/**
+	 *	Get the next SecUserIdGen identifier.
+	 *
+	 *	@return	The next SecUserIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextSecUserIdGen();
+
+	/**
+	 *	Get the next TenantIdGen identifier.
+	 *
+	 *	@return	The next TenantIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextTenantIdGen();
+
+	/**
+	 *	Get the next SecSysGrpIdGen identifier.
+	 *
+	 *	@return	The next SecSysGrpIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextSecSysGrpIdGen();
+
+	/**
+	 *	Get the next SecClusGrpIdGen identifier.
+	 *
+	 *	@return	The next SecClusGrpIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextSecClusGrpIdGen();
+
+	/**
+	 *	Get the next SecClusRoleIdGen identifier.
+	 *
+	 *	@return	The next SecClusRoleIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextSecClusRoleIdGen();
+
+	/**
+	 *	Get the next SecTentGrpIdGen identifier.
+	 *
+	 *	@return	The next SecTentGrpIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextSecTentGrpIdGen();
+
+	/**
+	 *	Get the next SecTentRoleIdGen identifier.
+	 *
+	 *	@return	The next SecTentRoleIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextSecTentRoleIdGen();
+
+	/**
+	 *	Get the next MajorVersionIdGen identifier.
+	 *
+	 *	@return	The next MajorVersionIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextMajorVersionIdGen();
+
+	/**
+	 *	Get the next MinorVersionIdGen identifier.
+	 *
+	 *	@return	The next MinorVersionIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextMinorVersionIdGen();
+
+	/**
+	 *	Get the next SubProjectIdGen identifier.
+	 *
+	 *	@return	The next SubProjectIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextSubProjectIdGen();
+
+	/**
+	 *	Get the next TldIdGen identifier.
+	 *
+	 *	@return	The next TldIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextTldIdGen();
+
+	/**
+	 *	Get the next TopDomainIdGen identifier.
+	 *
+	 *	@return	The next TopDomainIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextTopDomainIdGen();
+
+	/**
+	 *	Get the next TopProjectIdGen identifier.
+	 *
+	 *	@return	The next TopProjectIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextTopProjectIdGen();
+
+	/**
+	 *	Get the next LicenseIdGen identifier.
+	 *
+	 *	@return	The next LicenseIdGen identifier.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public CFLibDbKeyHash256 nextLicenseIdGen();
+
+	/**
+	 *	Get the factory for CFSecProt data objects.
+	 */
+	// public ICFSecProtFactory getCFSecProtFactory();
+
+	/**
+	 *	Get the buffer factory for CFSecProt data buffers.
+	 */
+	// public CFSecProtBuffFactoryService getCFSecProtBuffFactory();
+
+	/**
+	 *	Get the factory for CFIntProt data objects.
+	 */
+	// public ICFIntProtFactory getCFIntProtFactory();
+
+	/**
+	 *	Get the buffer factory for CFIntProt data buffers.
+	 */
+	// public CFIntProtBuffFactoryService getCFIntProtBuffFactory();
+
+	/**
+	 *	Get the License Table interface for the schema.
+	 *
+	 *	@return	The License Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtLicenseTable getTableLicense();
+
+	/**
+	 *	Get the MajorVersion Table interface for the schema.
+	 *
+	 *	@return	The MajorVersion Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtMajorVersionTable getTableMajorVersion();
+
+	/**
+	 *	Get the MimeType Table interface for the schema.
+	 *
+	 *	@return	The MimeType Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtMimeTypeTable getTableMimeType();
+
+	/**
+	 *	Get the MinorVersion Table interface for the schema.
+	 *
+	 *	@return	The MinorVersion Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtMinorVersionTable getTableMinorVersion();
+
+	/**
+	 *	Get the SubProject Table interface for the schema.
+	 *
+	 *	@return	The SubProject Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtSubProjectTable getTableSubProject();
+
+	/**
+	 *	Get the Tld Table interface for the schema.
+	 *
+	 *	@return	The Tld Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtTldTable getTableTld();
+
+	/**
+	 *	Get the TopDomain Table interface for the schema.
+	 *
+	 *	@return	The TopDomain Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtTopDomainTable getTableTopDomain();
+
+	/**
+	 *	Get the TopProject Table interface for the schema.
+	 *
+	 *	@return	The TopProject Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtTopProjectTable getTableTopProject();
+
+	/**
+	 *	Get the URLProtocol Table interface for the schema.
+	 *
+	 *	@return	The URLProtocol Table interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	public ICFIntProtURLProtocolTable getTableURLProtocol();
+
+	/**
+	 *	Get the Table Permissions interface for the schema.
+	 *
+	 *	@return	The Table Permissions interface for the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	//public static ICFSecProtTablePerms getTablePerms();
+
+	/**
+	 *	Get the Table Permissions interface cast to this schema's implementation.
+	 *
+	 *	@return The Table Permissions interface for this schema.
+	 */
+	//public static ICFSecProtTablePerms getCFSecProtTablePerms();
+
+	/**
+	 *	Set the Table Permissions interface for the schema.  All fractal subclasses of
+	 *	the ICFSecProtTablePerms implement at least that interface plus their own
+	 *	accessors.
+	 *
+	 *	@param	value	The Table Permissions interface to be used by the schema.
+	 *
+	 *	@throws CFLibNotSupportedException thrown by client-side implementations.
+	 */
+	//public static void setTablePerms( ICFSecProtTablePerms value );
+
+	public void bootstrapSchema(CFSecPubTableData tableData[]);
+	public void bootstrapAllTablesSecurity(CFLibDbKeyHash256 clusterId, CFLibDbKeyHash256 tenantId, CFSecPubTableData tableData[]);
+}
